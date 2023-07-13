c7107ce6855e0636d6694cbb6e04eefc2a60c701
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-13 15:57:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
  * main - prints "Programming is like building a multilingual
- *  puzzle,followed by a new line.
- *  Return Always  0.
+ *        puzzle", followed by a new line.
+ * Return: Always 0.
  */
 int main(void)
-  {
-	  puts("\""Programming is  like building a multilingual puzzle");
+{
+        puts("\"Programming is like building a multilingual puzzle");
 
-	  return (0);
-  }
+        return (0);
+}
+
