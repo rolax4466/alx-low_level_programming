c6158a0c19b7b341d4609131cb725eeaf969b24a
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-14 08:12:05 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* Printing numbers from 0 to 9 using putchar */
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+
+	putchar('\n');
+	return (0);
+}
+
