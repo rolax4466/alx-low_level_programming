5b39f883219739be4dc36630aa6312745c52fc1e
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-14 08:41:58 -0700

    v

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
@@ -19,7 +19,7 @@ int main(void)
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 
-			if (i != 99 || j != 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
