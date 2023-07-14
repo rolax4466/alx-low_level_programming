98af7051aa63d09793c2835a45b404a5c1c7a7c1
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-14 07:45:47 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints if number is positive, zero, or negative
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
+
