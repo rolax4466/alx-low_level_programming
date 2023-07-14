4bfc7a01037ab3c64d54030055fc50199080e841
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-14 08:16:32 -0700

    v

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-	/* Printing numbers from 0 to 9 using putchar */
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int number;
+
+	number = '0';
+
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
 
 	putchar('\n');
 	return (0);
