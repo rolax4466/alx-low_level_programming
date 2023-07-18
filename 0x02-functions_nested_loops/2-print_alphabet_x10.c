9f4d69a8dba74af9cd299b9f8cf10f6196772d87
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-18 06:30:45 -0700

    r

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a new line.
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a newline.
  */
 void print_alphabet_x10(void)
 {
