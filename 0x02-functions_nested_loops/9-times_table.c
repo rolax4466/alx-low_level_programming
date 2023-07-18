774ea97fdee9e9604b0d3762b0ae6876d6998f44
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-18 07:12:58 -0700

    g

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting with 0.
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+
+			if (column == 0)
+				_putchar('0');
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + product / 10);
+				_putchar('0' + product % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
