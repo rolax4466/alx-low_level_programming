8fb745134cca8c7668a9fa4a9455e8211d790e61
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-25 00:48:35 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,30 +1,12 @@
-#include <stdio.h>
+#include "strlen.h"
 
-/**
- * _strlen - Returns the length of a string
- * @s: Input string
- *
- * Return: Length of the string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-
-	return (length);
-}
-
-int main(void)
-{
-	char str[] = "Hello, World!";
-	int length = _strlen(str);
-
-	printf("Length of the string: %d\n", length);
-
-	return (0);
+int _strlen(char *s) {
+    int length = 0;
+    
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
+    
+    return length;
 }
