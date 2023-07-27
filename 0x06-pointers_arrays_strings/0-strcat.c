2c3480c4ea369696beddf7ab74eb0d80f21b692a
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-07-27 01:31:01 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,47 +1,20 @@
 #include <stdio.h>
 
-/**
- * _strcat - Function to concatenate two strings
- * @dest: Pointer to the destination string
- * @src: Pointer to the source string
- *
- * Return: Pointer to the concatenated string (dest)
- */
-char *_strcat(char *dest, char *src)
-{
-	char *original_dest = dest; /* Store the original pointer to dest */
-
-	/* Move the pointer dest to the end of the destination string */
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-
-	/* Copy characters from src to dest until the end of src is reached */
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-
-	/* Add the terminating null byte to the concatenated string */
-	*dest = '\0';
-
-	return original_dest; /* Return the pointer to the concatenated string */
-}
+char *_strcat(char *dest, char *src);
 
 int main(void)
 {
-	char dest[100] = "Hello, ";
-	char src[] = "world!";
+    char dest[50] = "Hello, ";
+    char src[] = "world!";
+    char *result = _strcat(dest, src);
 
-	printf("Before concatenation: %s\n", dest);
+    printf("%s\n", result);
 
-	/* Call the _strcat function to concatenate the two strings */
-	char *result = _strcat(dest, src);
+    return 0;
+}
 
-	printf("After concatenation: %s\n", result);
-
-	return 0;
+char *_strcat(char *dest, char *src)
+{
+    // Implementation of the _strcat function (as provided before)
+    // ...
 }
