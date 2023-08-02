cf6889ce1f12e6d37211f599d8fc146eac9546f3
rolax4466 <rolaxcheruiyot325@gmail.com>
2023-08-02 13:19:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
-// Rest of your code here...
 #include <stdio.h>
 
-void _puts_recursion(char *s) {
-    if (*s == '\0') { // Base case: if the current character is null terminator '\0', end the recursion
-        putchar('\n'); // Print a new line after the whole string has been printed
+void _puts_recursion(char *s)
+{
+    if (*s == '\0') // Base case: If the current character is the null terminator, end the recursion.
+    {
+        putchar('\n'); // Print a new line when the end of the string is reached.
         return;
     }
 
-    putchar(*s); // Print the current character
-    _puts_recursion(s + 1); // Recursively call the function with the next character in the string
+    putchar(*s); // Print the current character of the string.
+    _puts_recursion(s + 1); // Recursively call _puts_recursion with the next character in the string.
 }
